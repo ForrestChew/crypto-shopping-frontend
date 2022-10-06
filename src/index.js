ac3211cbd316462e9d017a33aa1a9a08706280f6
Forrest Chew <forrestchew@gmail.com>
2022-10-06 16:21:04 -0700

    Wrapped app in context determining whether a user is authenticated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import AuthedProvider from "./contexts/AuthedProvider";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthedProvider>
+        <App />
+      </AuthedProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
