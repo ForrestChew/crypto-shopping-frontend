70ee4d80f8f2ac472f9efb53e7278d2c3d147a71
Forrest Chew <forrestchew@gmail.com>
2022-10-07 15:24:44 -0700

    removed needless files

--- src/auth/useCreateUser.js
+++
@@ -1,8 +0,0 @@
-import { createUser } from "./api";
-
-export const useCreateUser = async (username, password, confirmedPassword) => {
-  if (password === confirmedPassword) {
-    const res = await createUser(username, password);
-    return res;
-  }
-};
--- src/auth/useLogin.js
+++
@@ -1,6 +0,0 @@
-import { login } from "./api";
-
-export const useLogin = async (username, password) => {
-  const res = await login(username, password);
-  return res;
-};
