e3ce6affad1c87c5165a938b82fed6495464b476
Forrest Chew <forrestchew@gmail.com>
2022-10-08 12:37:57 -0700

    Renamed for clarity

---
+++ src/api/posts.js
@@ -0,0 +1,31 @@
+import { CP_BACKEND_API_BASE_URL } from "../constants";
+export const login = async (username, password) => {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: JSON.stringify(
+      `grant_type=&username=${username}&password=${password}&scope=&client_id=&client_secret=`
+    ),
+  };
+  try {
+    const res = await fetch(`${CP_BACKEND_API_BASE_URL}/login`, requestOptions);
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const createUser = async (username, password, confirmedPassword) => {
+  if (password !== confirmedPassword) return;
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: username, password }),
+  };
+  try {
+    const res = await fetch(`${CP_BACKEND_API_BASE_URL}/users`, requestOptions);
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
