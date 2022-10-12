9a5b83b42323d721edeb638a3ca5f5324ea8fdab
Forrest Chew <forrestchew@gmail.com>
2022-10-12 14:46:47 -0700

    Sets the context based on whether there is a user currently signed in

--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -1,12 +1,27 @@
-import { useState, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
+import { getCurrentUser } from "../api/gets";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState({
     isAuthed: false,
-    userId: null,
+    userId: "",
+    userEmail: "",
   });
+
+  useEffect(() => {
+    (async () => {
+      const currentUser = await getCurrentUser();
+      if (currentUser.id)
+        setUser({
+          ...user,
+          isAuthed: true,
+          userId: currentUser.id,
+          userEmail: currentUser.email,
+        });
+    })();
+  }, []);
 
   return (
     <UserContext.Provider value={[user, setUser]}>
