2c529c4f92be37b23e3f969143da00cc1329b458
Forrest Chew <forrestchew@gmail.com>
2022-10-19 17:18:11 -0700

    Added fetch reqs to rest API server

--- src/api/gets.js
+++ src/api/gets.js
@@ -5,16 +5,83 @@ export const getCurrentUser = async () => {
     method: "GET",
     credentials: "include",
   };
-  try {
-    const response = await fetch(
-      `${CP_BACKEND_API_BASE_URL}/users`,
-      requestOptions
-    );
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return await response.json();
-  } catch {
-    return { isSuccessfull: false };
+  const response = await fetch(
+    `${CP_BACKEND_API_BASE_URL}/users`,
+    requestOptions
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
   }
+  return await response.json();
+};
+
+export const getProductImage = async (imageTitle) => {
+  const requestOptions = {
+    method: "GET",
+  };
+  const response = await fetch(
+    `${CP_BACKEND_API_BASE_URL}/products/images/${imageTitle}`,
+    requestOptions
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response;
+};
+
+export const getProducts = async () => {
+  const requestOptions = {
+    method: "GET",
+    credentials: "include",
+  };
+  const response = await fetch(
+    `${CP_BACKEND_API_BASE_URL}/products`,
+    requestOptions
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
+};
+
+export const getTopDeals = async (skip = 0) => {
+  const requestOptions = {
+    method: "GET",
+  };
+  const response = await fetch(
+    `${CP_BACKEND_API_BASE_URL}/products/top-deals?skip=${skip}`,
+    requestOptions
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
+};
+
+export const getTotalTopDeals = async () => {
+  const requestOptions = {
+    method: "GET",
+  };
+  const response = await fetch(
+    `${CP_BACKEND_API_BASE_URL}/products/top-deals/total`,
+    requestOptions
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
+};
+
+export const getSingleProduct = async (productId) => {
+  const requestOptions = {
+    method: "GET",
+  };
+  const response = await fetch(
+    `${CP_BACKEND_API_BASE_URL}/products/${productId}`,
+    requestOptions
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
 };
