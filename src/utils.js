26da6137d392d47afa48b0489fb1ca5dbccbde41
Forrest Chew <forrestchew@gmail.com>
2022-10-26 14:20:41 -0700

    added top deal price converter

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,24 @@
 export const capitalizeStr = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  if (string) return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
 export const numToPrice = (number) => {
-  const numToArr = number.toString().split("");
-  let cntTillComma = 0;
-  for (let i = numToArr.length - 1; i > 0; i--) {
-    cntTillComma++;
-    if (cntTillComma === 3 && numToArr[i] !== ".") {
-      numToArr.splice(i, 0, ",");
-      cntTillComma = 0;
+  if (number) {
+    const numToArr = number.toString().split("");
+    let cntTillComma = 0;
+    for (let i = numToArr.length - 1; i > 0; i--) {
+      cntTillComma++;
+      if (cntTillComma === 3 && numToArr[i] !== ".") {
+        numToArr.splice(i, 0, ",");
+        cntTillComma = 0;
+      }
     }
+    numToArr.unshift("$");
+    const price = numToArr.join("");
+    return price;
   }
-  numToArr.unshift("$");
-  const price = numToArr.join("");
-  return price;
+};
+
+export const priceToTopDealPrice = (price) => {
+  if (price) return numToPrice((price * 0.8).toFixed(2));
 };
