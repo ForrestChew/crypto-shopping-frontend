cf25061a311543fbf1d0cf6acf5ddc13f8654698
Forrest Chew <forrestchew@gmail.com>
2022-10-31 11:20:15 -0700

    Added function to add item to cart

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,1 +1,21 @@
 import { CP_BACKEND_API_BASE_URL } from "../constants";
+
+export const createCartItem = async (productId, productQty) => {
+  const csrfAccessToken = document.cookie.split("=")[1].split(";")[0];
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRF-TOKEN": `${csrfAccessToken}`,
+    },
+    credentials: "include",
+  };
+  const response = await fetch(
+    `${CP_BACKEND_API_BASE_URL}/carts/${productId}/${productQty}`,
+    options
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
+};
