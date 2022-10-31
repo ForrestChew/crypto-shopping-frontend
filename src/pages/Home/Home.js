330e060d1bec20cab5f9a13bb8f16d5da12a11ce
Forrest Chew <forrestchew@gmail.com>
2022-10-31 11:16:10 -0700

    Created home/landing page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,20 @@
+import shopBags from "../../assets/shopping-bags-svg.svg";
+import Footer from "../../components/Footer/Footer";
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="home">
+        <div className="home__heading-container">
+          <h1 className="home__heading">Making Online Shopping</h1>
+          <h1 className="home__heading">Simple, since 2022.</h1>
+        </div>
+        <img id="shop-bags-icon" src={shopBags} alt="" />
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
