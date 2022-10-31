9c8f47ffa2e222d6a97db61b48e9cc55061832f8
Forrest Chew <forrestchew@gmail.com>
2022-10-31 11:21:42 -0700

    Added ability to remove item from cart

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,4 +1,5 @@
 import { useCart } from "./useCart";
+import Footer from "../../components/Footer/Footer";
 import {
   cartPriceIncludingTax,
   priceToTopDealPrice,
@@ -6,44 +7,57 @@ import {
 } from "../../utils";
 import "./Cart.css";
 const Cart = () => {
-  const { cartItems, totalCartPrice, multiplyByQuantity } = useCart();
+  const { cartItems, totalCartPrice, removeCartItem } = useCart();
   return (
     <>
-      <section className="cart-container">
-        <table className="cart-container__table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Qty</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartItems.map((item) => {
-              return (
-                <tr key={item.id} className="cart-contianer__entry">
-                  <td>{item.title}</td>
-                  <td>
-                    {item.is_top_deal
-                      ? priceToTopDealPrice(item.price)
-                      : item.price}
-                  </td>
-                  <td>{item.product_quantity}</td>
-                  <td>{item.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </section>
-      <section>
-        <h3>{numToPrice(totalCartPrice)} Total</h3>
-        <h2>
-          {cartPriceIncludingTax(totalCartPrice)} <em>(with 8% tax)</em>
-        </h2>
-        <button className="btn">Place Order</button>
-      </section>
+      <div className="cart">
+        <section className="cart-container">
+          <table className="cart-container__table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Unit Price</th>
+                <th>Qty</th>
+                <th>Category</th>
+                <th>Remove Item</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItems.map((item) => {
+                return (
+                  <tr key={item.id} className="cart-contianer__entry">
+                    <td>{item.title}</td>
+                    <td>
+                      {item.is_top_deal
+                        ? priceToTopDealPrice(item.price)
+                        : item.price}
+                    </td>
+                    <td>{item.product_quantity}</td>
+                    <td>{item.category}</td>
+                    <td>
+                      <button
+                        className="rem-btn"
+                        onClick={() => removeCartItem(item.id)}
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <section>
+            <h3>{numToPrice(totalCartPrice)} Total</h3>
+            <h2 className="cart-container__total-heading">
+              {cartPriceIncludingTax(totalCartPrice)}{" "}
+              <em>Total includes 8% tax</em>
+            </h2>
+            <button className="btn">Order</button>
+          </section>
+        </section>
+      </div>
+      <Footer />
     </>
   );
 };
